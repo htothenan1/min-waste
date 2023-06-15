63e945fd7173f5ae0b7b1dcd126b6784a3dea61e
Hernan Berisso <hernanberisso@hernanberisso.myfiosgateway.com>
2023-06-15 17:04:51 -0400

    feature/update name of item-016

--- src/app/components/EditItemForm.js
+++ src/app/components/EditItemForm.js
@@ -15,35 +15,65 @@ const EditItemForm = ({ item }) => {
     });
   };
 
-  const updateItem = async (data) => {
-    await updateItemAction(data.id, "updated");
+  const updateItem = async (data, newName) => {
+    await updateItemAction(data.id, newName);
+    toast.info(`Item updated!`, {
+      position: "top-center",
+      autoClose: 2000,
+    });
     setEditMode(!editMode);
+    setItemName("");
   };
 
   return (
     <div className="flex flex-col items-center border border-red-500 rounded-md mx-4 w-96">
       <h1 className="underline my-4">Item Details</h1>
+
       {item ? (
         <>
-          <h1 className="my-2">{item.name}</h1>
-          <h1 className="my-2">{`Expired: ${
-            item.expired ? "yes" : "Not yet"
+          {editMode ? (
+            <input
+              className="text-center"
+              placeholder={item.name}
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+          ) : (
+            <h1 className="my-2">{item.name}</h1>
+          )}
+
+          <h1 className="my-2">{`Expiration Date: ${
+            item.expired ? "yes" : "Not set"
           }`}</h1>
           <h1 className="my-2">{`Item Type: ${item.home}`}</h1>
-          <button
-            onClick={() => {
-              updateItem(item);
-            }}
-            className="border border-bg-slate-700 my-2 py-1 px-2 rounded-md bg-blue-500 text-white"
-          >
-            Update This Item
-          </button>
-          <button
-            onClick={() => deleteItem(item)}
-            className="border border-bg-slate-700 my-2 py-1 px-2 rounded-md bg-red-500 text-white"
-          >
-            Delete This Item
-          </button>
+          {editMode ? (
+            <button
+              onClick={() => {
+                updateItem(item, itemName);
+              }}
+              className="border border-bg-slate-700 my-2 py-1 px-2 rounded-md bg-blue-500 text-white"
+            >
+              Confirm Changes
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={() => {
+                setEditMode(true);
+              }}
+              className="border border-bg-slate-700 my-2 py-1 px-2 rounded-md bg-blue-500 text-white"
+            >
+              Update This Item
+            </button>
+          )}
+          {editMode ? null : (
+            <button
+              onClick={() => deleteItem(item)}
+              className="border border-bg-slate-700 my-2 py-1 px-2 rounded-md bg-red-500 text-white"
+            >
+              Delete This Item
+            </button>
+          )}
         </>
       ) : (
         <h1 className="text-center my-4">no item selected</h1>
