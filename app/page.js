6ed4bacdc029d9578781831008d425b863c5e557
Hernan Berisso <hberisso1@gmail.com>
2023-06-23 13:22:07 -0400

    fix linting errors for deployment-30

--- app/page.js
+++ app/page.js
@@ -16,24 +16,25 @@ export default async function Home() {
           <p className="my-2 text-sm">
             To empower users to live a minimum-waste life. My hope is to provide
             an effective, easy-to-use tool to track the current contents of a
-            user's kitchen. Simply knowing what food you have in your kitchen
-            has been shown to reduce an individual's food waste significantly.
-            Additionally, I will develop features that teach the user about the
-            minimum-waste lifestyle, and how to apply it.
+            user&apos;s kitchen. Simply knowing what food you have in your
+            kitchen has been shown to reduce an individual&apos;s food waste
+            significantly. Additionally, I will develop features that teach the
+            user about the minimum-waste lifestyle, and how to apply it.
           </p>
           <h1 className="text-lg text-slate-600 font-semibold">
-            What's so bad about food waste?
+            What&apos;s so bad about food waste?
           </h1>
           <p className="my-2 text-sm">
             Nearly 40 percent of food in the US goes to waste! At the same time,
-            1 in 8 Americans don't have enough food to eat each day. Isn't that
-            absurd? That wasted food also makes up 20 percent of all landfills,
-            and ends up being a terrible producer of greenhouse gasses. The
-            resources that are spent to produce and deliver uneaten food to
-            consumers are immense. The only way this problem goes away is if we
-            can collectively improve our behavior, and stop wasting so much damn
-            food. Additionally, on a less altruistic note, the less food you
-            waste, the more money you save. So let's do this thing!
+            1 in 8 Americans don&apos;t have enough food to eat each day.
+            Isn&apos;t that absurd? That wasted food also makes up 20 percent of
+            all landfills, and ends up being a terrible producer of greenhouse
+            gasses. The resources that are spent to produce and deliver uneaten
+            food to consumers are immense. The only way this problem goes away
+            is if we can collectively improve our behavior, and stop wasting so
+            much damn food. Additionally, on a less altruistic note, the less
+            food you waste, the more money you save. So let&apos;s do this
+            thing!
           </p>
           <h1 className="text-lg text-slate-600 font-semibold">
             What tech am I utilizing?
