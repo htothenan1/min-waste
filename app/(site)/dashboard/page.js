741e068c5c5384da2cc21f07e776ed0278579d67
Hernan Berisso <hberisso1@gmail.com>
2023-07-12 13:19:44 -0400

    cta-011

--- app/(site)/dashboard/page.js
+++ app/(site)/dashboard/page.js
@@ -8,27 +8,30 @@ import { authOptions } from "../../api/auth/[...nextauth]/route";
 export default async function Home() {
   const session = await getServerSession(authOptions);
   const { user } = await getUser(session.user.email);
+  const itemsCount = user?.itemsCounter;
 
   return (
     <>
       <NavBar user={user?.name} />
       <div className="bg-slate-50/50 mb-10">
-        <h1 className="text-center m-8">{`${user?.name} has consumed ${user?.itemsCounter} complete items!`}</h1>
+        <h1 className="text-center m-8">
+          {`${user?.name} has consumed ` + itemsCount + ` complete items!`}
+        </h1>
         <Kitchen items={user?.items} />
         <div className="flex flex-col w-3/4 m-auto pb-24 bg-slate-50/50">
           <h1 className="text-slate-600 text-center font-semibold mt-4 text-lg md:text-2xl">
             How to get the most out of Waste-Not
           </h1>
-          <p className="my-2 text-green-800 text-sm md:text-base">
+          <p className="my-2 text-green-800 text-sm md:text-base p-1">
             <span className=" font-extrabold">1. Log your kitchen items.</span>{" "}
             The more accurate you keep your Items List, the more effective the
             app will be. <br />{" "}
             <span className=" font-extrabold">
               2. Set a Use-By Date for your items.
             </span>{" "}
-            This should be based on your own intuition of when the item should
-            responsibly be consumed by, NOT based on whatever expiration code is
-            written on the package. <br />{" "}
+            This should be based on what YOU feel is appropriate, NOT whatever
+            expiration code is written on the package. Make sure to factor in
+            your lifestyle and schedule when setting this date. <br />{" "}
             <span className="font-extrabold">
               3. Consume your items from the top of your list, first.
             </span>{" "}
--- app/page.js
+++ app/page.js
@@ -43,18 +43,23 @@ export default async function Home() {
       </div>
       <div className="flex flex-col w-3/4 mx-auto mt-6 bg-slate-50/50">
         <h1 className="text-slate-600 text-center font-semibold mt-4 text-2xl md:text-3xl">
-          How to get the most out of Waste-Not
+          How to get the most out of{" "}
+          <span className="text-orange-600">Waste-Not</span>
         </h1>
         <p className="my-2 text-green-800 text-sm md:text-base">
           <span className=" font-extrabold">1. Log your kitchen items.</span>{" "}
           The more accurate you keep your Items List, the more effective the app
-          will be. <br />{" "}
+          will be.
+        </p>
+        <p className="my-2 text-green-800 text-sm md:text-base">
           <span className=" font-extrabold">
             2. Set a Use-By Date for your items.
           </span>{" "}
           This should be based on what YOU feel is appropriate, NOT whatever
           expiration code is written on the package. Make sure to factor in your
-          lifestyle and schedule when setting this date. <br />{" "}
+          lifestyle and schedule when setting this date.
+        </p>
+        <p className="my-2 text-green-800 text-sm md:text-base">
           <span className="font-extrabold">
             3. Consume your items from the top of your list, first.
           </span>{" "}
@@ -62,17 +67,18 @@ export default async function Home() {
           <span className="text-orange-600">
             Always eat what should be eaten next.
           </span>
-          <br />{" "}
+        </p>
+        <p className="my-2 text-green-800 text-sm md:text-base">
           <span className="font-extrabold">
             4. Update your Items List when an item is finished.
           </span>{" "}
           We will keep count of the items you fully consume, so that you can
           track your overall progress.
-          <br /> <span className="font-extrabold">
-            5. Take it with you.
-          </span>{" "}
-          Our app is mobile friendly, so use it as a grocery shopping tool.
-          Never double buy an item again!
+        </p>
+        <p className="my-2 text-green-800 text-sm md:text-base">
+          <span className="font-extrabold">5. Take it with you.</span> Our app
+          is mobile friendly, so use it as a grocery shopping tool. Never double
+          buy an item again!
         </p>
 
         <h1 className=" text-slate-600 font-semibold mt-4 text-lg text-2xl md:text-3xl text-center">
@@ -81,9 +87,9 @@ export default async function Home() {
         <p className="my-2 text-green-800 text-sm md:text-base">
           It&apos;s probably worse than you think. Nearly 40 percent of food in
           the US goes to waste! At the same time, 1 in 8 Americans don&apos;t
-          have enough food to eat each day. Isn&apos;t that absurd? That wasted
-          food also makes up 20 percent of all landfills, and produces extreme
-          amounts of methane (look up{" "}
+          have enough food to eat. Isn&apos;t that absurd? That wasted food also
+          makes up 20 percent of all landfills, which produces extreme amounts
+          of methane (look up{" "}
           <Link
             className="text-orange-600"
             href={
