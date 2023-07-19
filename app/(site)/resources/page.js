3978ec6287d851eed749aee9f6e44b0bc3d22583
Hernan Berisso <hberisso1@gmail.com>
2023-07-19 00:06:09 -0400

    added resources

--- app/(site)/resources/page.js
+++ app/(site)/resources/page.js
@@ -5,6 +5,9 @@ import Footer from "../../common/Footer";
 import refedLogo from "../../../public/refed-logo.jpeg";
 import foodBankLogo from "../../../public/food-bank.png";
 import farmlinkLogo from "../../../public/farmlink-project.png";
+import feedingAmericaLogo from "../../../public/feeding-america.png";
+import fwraLogo from "../../../public/fwra.jpg";
+import jamesBeardLogo from "../../../public/james-beard-foundation.png";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import Image from "next/image";
 import Link from "next/link";
@@ -92,6 +95,83 @@ export default async function Resources() {
                   <p class="leading-relaxed">
                     Their mission is to make the worlds abundance of produce
                     accessible to everyone.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+              <div class="flex relative">
+                <Image
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full max-h-64 object-cover object-center"
+                  src={feedingAmericaLogo}
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-orange-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
+                    <Link
+                      href={"https://www.feedingamerica.org/"}
+                      target="_blank"
+                    >
+                      LEARN MORE
+                    </Link>
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Feeding America
+                  </h1>
+                  <p class="leading-relaxed">
+                    Feeding America is the largest charity working to end hunger
+                    in the United States.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+              <div class="flex relative">
+                <Image
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full max-h-64 object-cover object-center"
+                  src={fwraLogo}
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-orange-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
+                    <Link
+                      href={"https://www.foodwastealliance.org/"}
+                      target="_blank"
+                    >
+                      LEARN MORE
+                    </Link>
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    Food Waste Reduction Alliance
+                  </h1>
+                  <p class="leading-relaxed">
+                    FWRA’s objectives are to reduce our environmental footprint
+                    while helping those suffering from hunger.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="lg:w-1/3 sm:w-1/2 p-4">
+              <div class="flex relative">
+                <Image
+                  alt="gallery"
+                  class="absolute inset-0 w-full h-full max-h-64 object-cover object-center"
+                  src={jamesBeardLogo}
+                />
+                <div class="px-8 py-10 relative z-10 w-full border-4 border-orange-200 bg-white opacity-0 hover:opacity-100">
+                  <h2 class="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
+                    <Link href={"https://www.jamesbeard.org/"} target="_blank">
+                      LEARN MORE
+                    </Link>
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    James Beard Foundation
+                  </h1>
+                  <p class="leading-relaxed">
+                    A nonprofit organization whose mission is to support the
+                    people behind America’s food culture
                   </p>
                 </div>
               </div>
