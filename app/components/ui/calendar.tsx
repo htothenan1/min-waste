bc09b881baa50ebeed499edf1cb9f00a5d829398
Hernan <hberisso1@gmail.com>
2023-10-18 15:08:52 -0400

    mike changes, pt 2

--- app/components/ui/calendar.tsx
+++ app/components/ui/calendar.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 import { DayPicker } from "react-day-picker"
 
-import { cn } from "@/app/lib/utils.ts"
+import { cn } from "@/app/lib/utils"
 import { cva } from "class-variance-authority"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
