c3aeb19ff4977c865ed1dc5a9ef7aa6f6de91442
Hernan <hberisso1@gmail.com>
2023-10-26 16:14:02 -0400

    remove tailwind from singleitem

--- app/components/SingleItem.js
+++ app/components/SingleItem.js
@@ -1,5 +1,10 @@
 "use client"
 
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import Calendar from "react-calendar"
+import { ReloadIcon } from "@radix-ui/react-icons"
+import { useSpring, a } from "@react-spring/web"
 import {
   deleteItemAction,
   incrementCounterAction,
@@ -9,11 +14,6 @@ import {
   incrementWasteCounterAction,
   incrementMistakeCounterAction,
 } from "../_actions"
-import { useState } from "react"
-import { useSession } from "next-auth/react"
-import Calendar from "react-calendar"
-import { ReloadIcon } from "@radix-ui/react-icons"
-import { useSpring, a } from "@react-spring/web"
 import styles from "./styles/singleItem.module.css"
 
 const SingleItemView = ({
@@ -30,6 +30,7 @@ const SingleItemView = ({
   const [wasteLoading, setWasteLoading] = useState(false)
   const [updateLoading, setUpdateLoading] = useState(false)
   const [flipped, set] = useState(false)
+
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
@@ -94,7 +95,7 @@ const SingleItemView = ({
             )}
           </button>
 
-          <button type="button" class={styles.cancelButton} onClick={onClose}>
+          <button class={styles.cancelButton} onClick={onClose}>
             Cancel
           </button>
         </div>
@@ -118,7 +119,7 @@ const SingleItemView = ({
             {deleteLoading ? (
               <ReloadIcon class={styles.reloadIcon} />
             ) : (
-              <>Nope!</>
+              <p>Nope!</p>
             )}
           </button>
 
@@ -127,13 +128,12 @@ const SingleItemView = ({
               deleteItemWithWaste(item)
             }}
             disabled={wasteLoading}
-            type="button"
             class={styles.redButton}
           >
             {wasteLoading ? <ReloadIcon class={styles.reloadIcon} /> : "Yes"}
           </button>
 
-          <button type="button" class={styles.cancelButton} onClick={onClose}>
+          <button class={styles.cancelButton} onClick={onClose}>
             Cancel
           </button>
         </div>
@@ -155,38 +155,32 @@ const SingleItemView = ({
         ></MistakenModal>
       )}
 
-      <div className="flex flex-col m-6">
-        <h2 className="text-left pb-2 font-quicksandBold text-lg text-slate-600">
-          Item Details
-        </h2>
+      <div class={styles.itemDetailsContainer}>
+        <h2 class={styles.titleText}>Item Details</h2>
 
-        <div className="flex items-center justify-center">
+        <div class={styles.cardContainer}>
           <a.div
-            className="flex flex-col z-10 items-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-300/50 via-slate-100/50 to-indigo-100/50 shadow-2xl rounded-lg w-96 h-72"
+            class={styles.sideA}
             style={{ opacity: opacity.to((o) => 1 - o), transform }}
           >
             {item ? (
               <>
                 <button
-                  className="bg-white rounded-b-md p-1 text-sm font-quicksand hover:bg-slate-50 active:bg-slate-100"
+                  class={styles.storageTipsButton}
                   onClick={() => set((state) => !state)}
                 >
                   Click for Storage Tips
                 </button>
-                <h2 className="mt-2 text-slate-600 text-lg font-quicksandBold cursor-default">
-                  {item.name}
-                </h2>
+                <h2 class={styles.itemNameTitle}>{item.name}</h2>
                 {editStatus ? (
-                  <div>
-                    <Calendar
-                      minDate={new Date()}
-                      className={` rounded-lg text-sm`}
-                      onChange={onChange}
-                      value={value}
-                    />
-                  </div>
+                  <Calendar
+                    minDate={new Date()}
+                    className={styles.calendar}
+                    onChange={onChange}
+                    value={value}
+                  />
                 ) : (
-                  <h2 className="text-slate-600 cursor-default mb-2 font-quicksand">{`Use By: ${
+                  <h2 class={styles.itemDateText}>{`Use By: ${
                     item.expiredAt
                       ? item.expiredAt.toLocaleString("en-En", {
                           weekday: "short",
@@ -202,17 +196,10 @@ const SingleItemView = ({
                     onClick={() => {
                       updateItem(item)
                     }}
-                    className={`shadow-lg my-3 py-2 px-3 rounded-md ${
-                      updateLoading
-                        ? "bg-slate-400 hover:bg-slate-500 active:bg-slate-600"
-                        : "bg-blue-500/80 hover:bg-blue-600/80 active:bg-blue-700/80"
-                    }   text-white text-sm`}
+                    class={styles.confirmButton}
                   >
                     {updateLoading ? (
-                      <div className="flex justify-center items-center">
-                        Please Wait
-                        <ReloadIcon className="ml-2 h-4 w-4 animate-spin" />
-                      </div>
+                      <ReloadIcon class={styles.reloadIcon} />
                     ) : (
                       "Confirm Changes"
                     )}
@@ -220,11 +207,10 @@ const SingleItemView = ({
                 ) : (
                   <button
                     disabled={flipped}
-                    type="button"
                     onClick={() => {
                       handleEditToggle(true)
                     }}
-                    className="py-2 px-3 my-3 mt-3 rounded-md bg-blue-500/80 hover:bg-blue-600/80 active:bg-blue-700/80 text-white text-sm shadow-lg font-quicksand"
+                    class={styles.changeDateButton}
                   >
                     Change Date
                   </button>
@@ -234,14 +220,14 @@ const SingleItemView = ({
                     <button
                       disabled={flipped}
                       onClick={() => setFinishedModalOpen(true)}
-                      className="py-2 px-3 my-3 rounded-md bg-green-600/50 hover:bg-green-700/50 active:bg-green-800/50 text-white text-sm shadow-lg font-quicksand"
+                      class={styles.itemFinishedButton}
                     >
                       Item Finished
                     </button>
                     <button
                       disabled={flipped}
                       onClick={() => setMistakenModalOpen(true)}
-                      className=" text-xs text-red-500 text-center border border-red-500 py-2 px-3 my-3 rounded-md shadow-lg font-quicksand bg-white hover:bg-slate-100 active:bg-slate-200/50"
+                      class={styles.mistakeButton}
                     >
                       Added by mistake?
                     </button>
@@ -249,13 +235,11 @@ const SingleItemView = ({
                 )}
               </>
             ) : (
-              <h2 className="text-center my-auto cursor-default text-slate-600 font-quicksand">
-                No item selected
-              </h2>
+              <h2 class={styles.noItemText}>No item selected</h2>
             )}
           </a.div>
           <a.div
-            className="flex flex-col absolute items-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-300/50 via-slate-100/50 to-indigo-100/50 shadow-lg rounded-md w-96 h-72 overflow-scroll"
+            class={styles.sideB}
             style={{
               opacity,
               transform,
@@ -264,21 +248,15 @@ const SingleItemView = ({
           >
             {item ? (
               <>
-                <h2 className="mt-3 text-slate-600 text-lg font-quicksandBold cursor-default">
+                <h2 class={styles.storageTipsTitle}>
                   {`Storage Tips for ${item.name}:`}
                 </h2>
-                <p className="px-4 text-left font-quicksand ">
-                  {item.storageTip}
-                </p>
+                <p class={styles.storageTipsSub}>{item.storageTip}</p>
 
-                <button className="absolute bottom-0 bg-white rounded-t-md p-1 text-sm font-quicksand">
-                  Flip Back
-                </button>
+                <button class={styles.flipBackButton}>Flip Back</button>
               </>
             ) : (
-              <h2 className="text-center my-auto cursor-default text-slate-600 font-quicksand">
-                No item selected
-              </h2>
+              <h2 class={styles.noItemText}>No item selected</h2>
             )}
           </a.div>
         </div>
