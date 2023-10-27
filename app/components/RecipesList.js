a0ad850e0745038d8e5d28acd4f688cc8ec2eb89
Hernan <hberisso1@gmail.com>
2023-10-27 10:50:56 -0400

    remove tailwind from recipeslist

--- app/components/RecipesList.js
+++ app/components/RecipesList.js
@@ -1,30 +1,25 @@
 "use client"
 
+import styles from "./styles/recipesList.module.css"
+
 const RecipesList = ({ recipes, handleSelectRecipe }) => {
   return (
-    <div className="m-6 flex flex-col text-left">
-      <h2 className="text-left pb-2 font-quicksandBold text-lg text-slate-600">
-        Your Recipes
-      </h2>
+    <div class={styles.recipesListContainer}>
+      <h2 class={styles.titleText}>Your Recipes</h2>
 
-      <ul
-        role="list"
-        className="flex flex-col h-96 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-300/50 via-slate-100/50 to-indigo-100/50 shadow-2xl rounded-lg overflow-scroll w-96"
-      >
+      <ul role="list" class={styles.recipesList}>
         {recipes && recipes.length > 0 ? (
           recipes.map((recipe) => (
             <li
               onClick={() => handleSelectRecipe(recipe.id)}
               key={recipe.id}
-              className="relative shadow-lg cursor-pointer px-4 py-4 bg-orange-200/50 hover:bg-orange-100/50 rounded-md font-quicksandBold text-sm font-medium text-slate-600"
+              class={styles.recipeItem}
             >
               {recipe.title}
             </li>
           ))
         ) : (
-          <p className="p-2 cursor-default my-auto mx-auto text-slate-600 font-quicksand">
-            No current recipes
-          </p>
+          <p class={styles.emptyListText}>No current recipes</p>
         )}
       </ul>
     </div>
