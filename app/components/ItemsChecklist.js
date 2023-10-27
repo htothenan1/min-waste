6747ae9c114bad8a4e839009411c27cabecac6c3
Hernan <hberisso1@gmail.com>
2023-10-27 08:12:11 -0400

    remove tailwind from recipes page

--- app/components/ItemsChecklist.js
+++ app/components/ItemsChecklist.js
@@ -4,6 +4,7 @@ import { useState } from "react"
 import RecipesList from "./RecipesList"
 import SingleRecipe from "./SingleRecipe"
 import { toast } from "react-toastify"
+import styles from "./styles/itemsChecklist.module.css"
 
 const ItemsChecklist = ({ items }) => {
   const [fetchedRecipes, setFetchedRecipes] = useState(null)
@@ -18,12 +19,10 @@ const ItemsChecklist = ({ items }) => {
         (data.expiredAt.getTime() - new Date().getTime()) / (1000 * 3600 * 24)
 
       if (daysFrom < 2) {
-        return `bg-red-300/30 hover:bg-red-200`
+        return styles.redItem
       } else {
-        return `bg-green-300/30 hover:bg-green-200/30`
+        return styles.greenItem
       }
-    } else {
-      return `bg-slate-200/30 hover:bg-slate-100/30`
     }
   }
 
@@ -45,18 +44,26 @@ const ItemsChecklist = ({ items }) => {
         itemsArray.push(items[i])
       }
     }
-    const queryString = itemsArray.map((item) => item.name).join(",+")
-    fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=757d368ebb304fb3bf99a64e38c11942&ingredients=${queryString}&number=15`
-    )
-      .then((response) => response.json())
-      .then((resItems) => {
-        toast.success("Recipes found!", {
-          position: "top-center",
-          autoClose: 1000,
+
+    if (itemsArray.length) {
+      const queryString = itemsArray.map((item) => item.name).join(",+")
+      fetch(
+        `https://api.spoonacular.com/recipes/findByIngredients?apiKey=757d368ebb304fb3bf99a64e38c11942&ingredients=${queryString}&number=15`
+      )
+        .then((response) => response.json())
+        .then((resItems) => {
+          toast.success("Recipes found!", {
+            position: "top-center",
+            autoClose: 1000,
+          })
+          setFetchedRecipes(resItems)
         })
-        setFetchedRecipes(resItems)
+    } else {
+      toast.error("Select some items first!", {
+        position: "top-center",
+        autoClose: 1000,
       })
+    }
   }
 
   const handleSelectRecipe = (data) => {
@@ -71,56 +78,47 @@ const ItemsChecklist = ({ items }) => {
   }
 
   return (
-    <div className="mt-2 mb-4">
-      <div className="flex flex-col justify-center items-center">
-        <button
-          onClick={() => resetSelectedItems()}
-          className="py-1 px-2 rounded-md bg-blue-600/60 text-white text-sm font-quicksand shadow-2xl active:bg-blue-700/80 w-32"
-        >
-          Clear Selections
-        </button>
-        <button
-          onClick={() => fetchRecipes()}
-          className="py-1 px-2 rounded-md bg-orange-600/60 text-white text-sm font-quicksand shadow-2xl active:bg-red-700/80 w-32 my-2"
-        >
-          Get Recipes
-        </button>
-      </div>
+    <>
+      <button
+        disabled={!checkedState}
+        onClick={() => fetchRecipes()}
+        class={styles.getRecipesButton}
+      >
+        Get Recipes
+      </button>
+      <button onClick={() => resetSelectedItems()} class={styles.clearButton}>
+        Clear Selections
+      </button>
 
-      <h2 className="text-left mb-2 ml-4 font-quicksandBold text-lg text-slate-600">
-        Your Items
-      </h2>
+      <h2 class={styles.titleText}>Your Items</h2>
 
       {items.length ? (
-        <ul className="grid grid-cols-3 md:grid-cols-6 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-300/50 via-slate-100/50 to-indigo-100/50 shadow-2xl rounded-lg mb-10 mx-4">
+        <div class={styles.itemsGrid}>
           {items.map((item, index) => (
             <div
               onClick={() => handleItemSelect(index)}
               key={item.id}
-              className={`${calcDaysFrom(item)} ${
-                checkedState[index] === true ? "bg-white hover:bg-white" : ""
-              } relative cursor-pointer shadow-lg px-4 py-3 
-            focus-within:ring-2 focus-within:ring-green-200 rounded-md`}
+              class={`${calcDaysFrom(item)} ${
+                checkedState[index] === true && styles.orangeBackground
+              }`}
             >
-              <p
-                className={`truncate text-sm font-medium text-slate-600 font-quicksandBold`}
-              >
-                {item.name}
-              </p>
+              <p>{item.name}</p>
             </div>
           ))}
-        </ul>
+        </div>
       ) : (
-        <p>No Items! Log some items to get started!</p>
+        <p class={styles.emptyListText}>
+          No Items! Log some items to get started!
+        </p>
       )}
-      <div className="flex justify-center flex-wrap">
+      <div class={styles.recipeCardsContainer}>
         <RecipesList
           recipes={fetchedRecipes}
           handleSelectRecipe={handleSelectRecipe}
         />
         <SingleRecipe recipe={selectedRecipe} />
       </div>
-    </div>
+    </>
   )
 }
 
