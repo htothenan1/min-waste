d67b22182fbefb700c20ff980f9a167824ce2b61
Hernan <hberisso1@gmail.com>
2023-10-26 21:55:29 -0400

    remove tailwind from contact form

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -1,96 +1,56 @@
+import styles from "./styles/contactForm.module.css"
+
 export default function ContactForm() {
   return (
-    <div className="bg-slate-50 px-6 pt-12 pb-24 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-blue-50 opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-quicksandBold text-orange-600/70">
-          Contact us
-        </h2>
-        <p className="mt-2 text-md text-slate-600 font-quicksandBold">
-          Have an idea for a great new feature? We&apos;d love to hear about it!
-          Found a bug? Help squash it by reporting it in detail!
+    <div class={styles.contactContainer}>
+      <div class={styles.headerContainer}>
+        <h2 class={styles.titleText}>Contact us</h2>
+        <p class={styles.subText}>
+          Have a feature idea? Found a bug? Let us know!
         </p>
       </div>
       <form
         action="https://formsubmit.co/hberissodev@gmail.com"
         method="POST"
-        className="mx-auto mt-16 max-w-xl sm:mt-20"
+        class={styles.formContainer}
       >
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-quicksandBold leading-6 text-slate-600"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                required
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-slate-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+        <label htmlFor="first-name" class={styles.labelText}>
+          First name
+        </label>
+        <input
+          type="text"
+          name="first-name"
+          id="first-name"
+          autoComplete="given-name"
+          required
+          class={styles.input}
+        />
 
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-quicksandBold leading-6 text-slate-600"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-slate-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+        <label htmlFor="email" class={styles.labelText}>
+          Email
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          autoComplete="email"
+          class={styles.input}
+          required
+        />
 
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-quicksandBold leading-6 text-slate-600"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                required
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-slate-600 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6 font-quicksand"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-orange-600/70 px-3.5 py-2.5 text-center text-sm font-quicksandBold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
-          >
-            Send Message
-          </button>
-        </div>
+        <label htmlFor="message" class={styles.labelText}>
+          Message
+        </label>
+        <textarea
+          required
+          name="message"
+          id="message"
+          rows={4}
+          class={styles.textArea}
+        />
+        <button type="submit" class={styles.sendButton}>
+          Send Message
+        </button>
       </form>
     </div>
   )
