b219a8ab715714b89c664d7d6c267c81e3f496a1
Hernan <hberisso1@gmail.com>
2023-11-08 17:47:05 -0500

    first pass vision testing!

--- app/components/AccountDetails.js
+++ app/components/AccountDetails.js
@@ -1,14 +1,73 @@
 "use client"
 
-import { deleteItemsActions } from "@/_actions"
+import {
+  createItemAction,
+  deleteItemsActions,
+  incrementLogCounterAction,
+} from "@/_actions"
+import { addDays } from "date-fns"
+import { useSession } from "next-auth/react"
 import { useState } from "react"
+import { generateStorageTip, receiptTest, veggiesTest } from "@/utils/openai"
 import styles from "./styles/accountDetails.module.css"
+import { ReloadIcon } from "@/data/icons"
 
 const AccountDetails = ({ user }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const { data: session } = useSession()
+  const [file, setFile] = useState(undefined)
+  const [itemList, setItemList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [openLoading, setOpenLoading] = useState(false)
   const deleteAllItems = async () => {
     await deleteItemsActions(user.id)
     setIsOpen(false)
+  }
+
+  const addCustomItem = async (clientData) => {
+    const tip = await generateStorageTip(clientData)
+    const fiveDaysFromToday = addDays(new Date(), 5)
+    await createItemAction(
+      session.user.email,
+      clientData,
+      tip,
+      fiveDaysFromToday
+    )
+    await incrementLogCounterAction(session.user.email)
+  }
+
+  const handleAddItems = () => {
+    handleVeggies()
+    itemList.forEach((item) => {
+      addCustomItem(item)
+    })
+  }
+
+  const confirmAddItems = async () => {
+    setLoading(true)
+    try {
+      await Promise.all(itemList.map((item) => addCustomItem(item)))
+    } catch (error) {
+      console.error("Error adding items:", error)
+    }
+    setLoading(false)
+  }
+
+  const handleVeggies = async () => {
+    setOpenLoading(true)
+    const items = await veggiesTest(file)
+    const itemsArray = items.message.content.split(",")
+    setItemList(itemsArray)
+    setOpenLoading(false)
+  }
+
+  const handleOnChange = (e) => {
+    const reader = new FileReader()
+
+    reader.addEventListener("load", () => {
+      setFile(reader.result)
+    })
+    reader.readAsDataURL(e.target.files[0])
   }
 
   function Modal({ onClose }) {
@@ -30,6 +89,7 @@ const AccountDetails = ({ user }) => {
           >
             Delete All Items
           </button>
+
           <button className={styles.cancelButton} onClick={onClose}>
             Cancel
           </button>
@@ -51,6 +111,34 @@ const AccountDetails = ({ user }) => {
         onClick={() => setIsOpen(true)}
       >
         Clear Fridge Items
+      </button>
+      <input type="file" onChange={(e) => handleOnChange(e)} />
+      <button
+        className={styles.clearFridgeButton}
+        onClick={() => handleVeggies()}
+      >
+        {openLoading ? (
+          <ReloadIcon className={styles.reloadIcon} />
+        ) : (
+          "Analyze Photo"
+        )}
+      </button>
+      {/* <button
+        className={styles.clearFridgeButton}
+        onClick={() => receiptTest(file)}
+      >
+        Receipt Test
+      </button> */}
+      {/* <button
+        className={styles.greenButton}
+        onClick={() => {
+          console.log(itemList)
+        }}
+      >
+        console Log File
+      </button> */}
+      <button className={styles.greenButton} onClick={() => confirmAddItems()}>
+        {loading ? <ReloadIcon className={styles.reloadIcon} /> : "Add Items"}
       </button>
       <h2 className={styles.accountRegularText}>
         Total Items Logged: {user.loggedCounter - user.mistakeCounter}
--- app/utils/openai.js
+++ app/utils/openai.js
@@ -1,5 +1,7 @@
 "use server"
 
+const fs = require("fs")
+
 import OpenAI from "openai"
 
 const openai = new OpenAI({
@@ -19,4 +21,50 @@ export async function generateStorageTip(item) {
   })
 
   return completion.choices[0].message.content
+}
+
+export async function receiptTest(imgFile) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Identify the items on this receipt as common grocery items. Exclude snacks and non-food items that you identify. Seperate each item with a comma. Make sure the items do not include any adjectives or qualifiers such as “organic” or “half gallon”. Make sure there are no repeated items on the list you give me. Make sure the vegetable and fruit items are plural, as long as it makes grammatical sense. Make sure all items are capitalized, and limited to one word if possible. Just give me the items, with no other words.",
+          },
+          {
+            type: "image_url",
+            image_url: `${imgFile}`,
+          },
+        ],
+      },
+    ],
+    max_tokens: 300,
+  })
+  console.log(response.choices[0])
+}
+
+export async function veggiesTest(imgFile) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Can you list the vegetables and fruits in this picture, seperated by commas. Please capitalize each item and do not include any other words in your response. Please make sure items any sort of adjectives.",
+          },
+          {
+            type: "image_url",
+            image_url: `${imgFile}`,
+          },
+        ],
+      },
+    ],
+    max_tokens: 300,
+  })
+  return response.choices[0]
 }
